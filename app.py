01239e08948bf7b78ad65fac6b02fc3078af8245
4kiUta <st.jose.alexandre@gmail.com>
2022-08-10 23:33:32 +0100

    extra comment

--- app.py
+++ app.py
@@ -12,5 +12,6 @@ of all the cool things
 
 
 ## playing around
+## for better charts might be needed to install 'plotly' module
 data = pd.read_csv('data_gen/team_heart.csv')
 st.area_chart(data=data['Age'])
