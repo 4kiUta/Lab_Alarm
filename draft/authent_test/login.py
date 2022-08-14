d99d429d0561ac0aad46b31497c71600b0ed41f8
4kiUta <st.jose.alexandre@gmail.com>
2022-08-14 18:07:40 +0100

    understanding register and login  ### login doesnt run withou register first

---
+++ draft/authent_test/login.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+# import pyyaml module
+import yaml
+from yaml.loader import SafeLoader
+
+with open('users/config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+
+if st.session_state["authentication_status"]:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] == False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] == None:
+    st.warning('Please enter your username and password')
+
